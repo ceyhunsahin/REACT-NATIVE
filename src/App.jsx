12591959f6c43e70998a4bda2ff0fe2de499d511
ceyhunsahin <csahin17072010@gmail.com>
2024-05-01 23:34:17 +0200

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,87 @@
 import * as React from 'react';
-import { Button, View } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
       <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', {
+            
+            otherParam: 'anything you want here',
+          });
+        }}
       />
     </View>
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId, otherParam } = route.params;
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {otherParam}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+            otherParam: 'ben bunu guncelledim',
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Update the title"
+        onPress={() => navigation.setOptions({ title: 'Updated!' })}
+      />
     </View>
   );
 }
 
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator initialRouteName="Home"
+      screenOptions={{
+       
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }} >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} 
+        initialParams={{ itemId: 42 }}
+        //options={{
+          //headerTitle: (props) => <LogoTitle {...props} />,
+          //headerRight: () => (
+          //  <Button
+           //   onPress={() => alert('This is a button!')}
+          //    title="Info"
+          //    color="#fff"
+         //   />)
+        //}}
+         options={{ headerShown: false }}
+        //options={({ route }) => ({ title: route.params.name }) 
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
